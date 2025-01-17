9a264d0d6bfc876a0094c9a5792133b48721b2f9
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-01-17 16:06:01 +0100

    removed unused service

--- backend/src/main/java/org/neuefische/backend/service/BookService.java
+++ backend/src/main/java/org/neuefische/backend/service/BookService.java
@@ -14,9 +14,6 @@ public class BookService {
         this.bookRepo = bookRepo;
     }
 
-    //public BookService(BookRepo bookRepo) {
-
-
     public List<Book> getAllBooks() {
         return bookRepo.findAll();
     }
